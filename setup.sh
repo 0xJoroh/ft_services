39d2c83072010be1051b47e1663973a99104d08c
Mohamed Ait si m'hand <mait-si-@e2r6p8.1337.ma>
2020-10-21 18:54:24 +0100

    commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
-docker build ./srcs/nginx/ -t nginx-img
+docker build -t nginx-img      ./srcs/nginx/
+docker build -t mysql-img      ./srcs/mysql/
+docker build -t phpmyadmin-img ./srcs/phpmyadmin/
+docker build -t wordpress-img  ./srcs/wordpress/
+
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
+kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+kubectl apply -f ./srcs/wordpress/wordpress.yaml
