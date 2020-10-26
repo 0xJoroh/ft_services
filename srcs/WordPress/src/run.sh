3feccf0445b02b16d1ce85c667deecba744cc58a
Mohamed Ait si m'hand <mait-si-@e2r7p6.1337.ma>
2020-10-26 14:40:31 +0100

    Wordpress image not working yet

---
+++ srcs/WordPress/src/run.sh
@@ -0,0 +1,2 @@
+# mysql --host=mysql-service --user=root --password=root wordpress < /tmp/wordpress.sql
+php -S 0.0.0.0:5050 -t /etc/wordpress/
--- srcs/WordPress/src/wp-config.php
+++ srcs/WordPress/src/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'DB_NAME' );
+define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'DB_USER' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'DB_PASSWORD' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'DB_HOST' );
+define( 'DB_HOST', 'mysql-service' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
