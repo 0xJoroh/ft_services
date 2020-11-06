aaab04fd83710475499b62ad1f1c7b9a5d4da754
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2020-11-06 16:21:56 +0100

    .

--- resetup.sh
+++ resetup.sh
@@ -1,3 +1,5 @@
 kubectl delete deployment wordpress-deployment mysql-deployment phpmyadmin-deployment nginx-deployment
 kubectl delete service mysql-service wordpress-service phpmyadmin-service nginx-service
+cclean
+iclean
 sh setup.sh
