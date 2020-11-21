28fc791eb8c3c4216addce2d2dd06c7bef25a94a
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2020-11-21 18:24:59 +0100

    .

--- srcs/WordPress/src/run.sh
+++ srcs/WordPress/src/run.sh
@@ -1,4 +1,2 @@
-# mysql --host=mysql-service --user=root --password=root wordpress < /tmp/wordpress.sql
-echo "CREATE DATABASE wordpress;" | mysql -u root --password=root
 php-fpm7
 nginx -g "daemon off;"
