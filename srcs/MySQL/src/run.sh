36c3d666796f9eec466e40271bbf2ed03d781ffe
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2020-11-25 18:55:17 +0100

    Mysql Image is ready

--- srcs/MySQL/src/run.sh
+++ srcs/MySQL/src/run.sh
@@ -6,8 +6,8 @@ rc-service mariadb start
 
 mysqladmin -u root password 'admin'
 mysql -u root -e "CREATE DATABASE wordpress;"
-mysql -u root -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%';"
+mysql -u root -e "CREATE USER admin@'%' IDENTIFIED BY 'admin';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO admin@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql wordpress -u root < wordpress.sql
 
