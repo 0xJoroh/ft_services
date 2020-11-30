8eaa5b1637aa130813ab07de2d06aba322ba6b60
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2020-11-30 13:53:56 +0100

    .

--- setup.sh
+++ setup.sh
@@ -18,9 +18,9 @@ docker build -t mysql-img        ./srcs/mysql/
 docker build -t phpmyadmin-img   ./srcs/phpmyadmin/
 docker build -t wordpress-img    ./srcs/wordpress/
 
-kubectl apply -f ./srcs/metallb_conf.yml
 kubectl apply -f ./srcs/mysql/mysql.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
 kubectl apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f ./srcs/wordpress/wordpress.yaml
+kubectl apply -f ./srcs/metallb_conf.yml
 
